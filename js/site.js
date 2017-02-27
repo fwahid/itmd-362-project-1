4531d4c8410eab2f406b9d5e6ece434e572780ba
fwahid <fwahid@hawk.iit.edu>
2017-02-27 10:14:46 -0600

    removed if statement for submit button

--- js/site.js
+++ js/site.js
@@ -7,7 +7,6 @@ $.noConflict();
         if($.trim($' && '#email').val()) === ' ') {
           alert('Please fill missing information');
         }
-        else if 
         $("#submit-button").click(function(){
         alert('Thank You! Your form has been submitted');
         }
