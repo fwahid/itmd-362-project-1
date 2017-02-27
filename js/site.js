b75f0df8c34bd26371cf103ae5a6f1cc8f14297a
fwahid <fwahid@hawk.iit.edu>
2017-02-27 10:07:02 -0600

    added alert message for no input in fields

--- js/site.js
+++ js/site.js
@@ -2,4 +2,9 @@ $.noConflict();
 (function($) {
   $(document).ready(function() {
     $('#form').on('submit', function(event) {
+      var input + $('#email').val();
+      console.log(input);
+        if($.trim($' && '#email').val()) === ' ') {
+          alert('Please fill missing information');
+        }
       event.preventDefault();
