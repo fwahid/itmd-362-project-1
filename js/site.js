8c381c48cceb129d7161d88579b505282ec0c49b
fwahid <fwahid@hawk.iit.edu>
2017-03-01 20:51:42 -0600

    removed radio type from biweekly

--- js/site.js
+++ js/site.js
@@ -1,16 +1,25 @@
-$.noConflict();
+//Prevents entire page from reloading on submit
+$(document).ready(function () {
+  $("#newsletter").on('submit', function (e) {
+    e.preventDefault();
+  })
+})
 
-jQuery(function($) {
-  $('#user-form').on('submit', function(event) {
-      
-    var emailInput = $('#email').val();
-    console.log(emailInput);
-    
-    if($.trim($('#email').val()) === '') {
-      alert('Please fill out all fields');
-    }
-      
-    event.preventDefault();
-      
-  });
-});
+function checkAll() {
+  //Resets output from previous check
+  document.getElementById("approvedx").innerHTML = "";
+  document.getElementById("agex").innerHTML = "";
+
+  var x = 0;
+
+  // AGE CHECK
+  if (document.getElementById("yesage").checked === false) {
+    document.getElementById("agex").innerHTML = "Please fill out all fields";
+    x = x + 1;
+  }
+  
+  // APPROVAL
+  if (x == 0) {
+    document.getElementById("approvedx").innerHTML = "Thank you for signing up!";
+  }
+}
