bab46f4c5fa394f33ea8d06a2ba93908adcfa146
fwahid <fwahid@hawk.iit.edu>
2017-03-01 21:00:14 -0600

    fixed spacing

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 //Prevents entire page from reloading on submit
-$(document).ready(function () {
-  $("#newsletter").on('submit', function (e) {
+$(document).ready(function() {
+  $("#newsletter").on('submit', function(e) {
     e.preventDefault();
   })
 })
@@ -9,15 +9,12 @@ function checkAll() {
   //Resets output from previous check
   document.getElementById("approvedx").innerHTML = "";
   document.getElementById("agex").innerHTML = "";
-
   var x = 0;
-
   // AGE CHECK
   if (document.getElementById("yesage").checked === false) {
     document.getElementById("agex").innerHTML = "Please fill out all fields";
     x = x + 1;
   }
-  
   // APPROVAL
   if (x == 0) {
     document.getElementById("approvedx").innerHTML = "Thank you for signing up!";
